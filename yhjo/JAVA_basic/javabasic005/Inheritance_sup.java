319880d1b9b73711c9e12ca1ace117ff4ec09399
yunhyun717 <yunhyun717@gmail.com>
2021-04-29 09:55:51 +0900

    update 0429

--- yhjo/JAVA_basic/javabasic005/Inheritance_sup.java
+++
@@ -1,9 +0,0 @@
-package javabasic005;
-
-public class Inheritance_sup {
-	public static int power = 40;
-
-	public void printPower() {
-		System.out.println(power);
-	}
-}
--- yhjo/JAVA_basic/javabasic005/Man.java
+++
@@ -1,9 +0,0 @@
-package javabasic005;
-
-public class Man extends People {
-
-	public void PrintContent() {
-		System.out.println("Man");
-	}
-
-}
